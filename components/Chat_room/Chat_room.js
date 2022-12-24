7c495c15f2973a80d56bbdd7840a5f35215a9f21
Your Name <you@example.com>
2022-12-24 03:04:53 -0800

    first commit

--- components/Chat_room/Chat_room.js
+++ components/Chat_room/Chat_room.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Loader from './Loader'
 
 export default function Chat_room() {
   return (
-    <div>Chat_room</div>
+    <main className='chat_room'>
+      <div className='loader'>
+       <Loader/>
+      <h1>connecting to the Server</h1>
+      </div>
+      <div className='input'>
+        <button>ESC</button>
+    <input/>
+    <button>send</button>
+      </div>
+    </main>
   )
 }
