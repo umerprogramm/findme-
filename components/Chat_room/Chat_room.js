1eed0b27cb5ee58ca5e3707c0f0b550f0c4f2e9b
Your Name <you@example.com>
2022-12-29 20:22:28 +0500

    I added some socket.io

--- components/Chat_room/Chat_room.js
+++ components/Chat_room/Chat_room.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import Loader from './Loader'
+import io from 'socket.io-client'
+let socket;
+
 
 export default function Chat_room() {
+  const [data , setData] = useState([])
+  const [input, setInput] = useState('')
+  const [sender , setsender ] = useState(false)
+    useEffect(() => {
+    const socketInitializer = async () => {
+      await fetch('/api/hello');
+      socket = io()
+  
+      socket.on('connect', () => {
+        console.log('connected')
+      })
+      socket.on('update-input',(msg )=>{
+        setData(msg)
+   
+      })
+     
+    }
+    socketInitializer();
+   
+  })
+  const Message_sender = ()=>{
+
+    socket.emit('input-change',[...data,input] )
+    setData([...data,input])
+   
+    setInput('')
+    setsender(true)
+  }
+  const onChangeHandler = (e) => {
+    setInput(e.target.value)
+  }
   return (
     <>
     <Navbar/>
@@ -11,10 +45,35 @@ export default function Chat_room() {
        <Loader/>
       <h1>connecting to the Server</h1>
       </div>
+      {/* {
+        chat.map(value =>{
+          return(
+            <ul>
+            <li>
+             
+  
+             
+            {sender ? <span style={{textAlign  : 'right'}}>{value}</span>:<span>{value}</span>}
+            </li>
+          </ul>
+          )
+         
+        })
+        
+
+      }
+       */}
+       {data.map((data) => (
+        <li>{data}</li>
+      ))}
       <div className='input'>
         <button>ESC</button>
-    <input/>
-    <button>send</button>
+        <input
+      placeholder="Type something"
+      value={input}
+      onChange={onChangeHandler}
+    />
+    <button onClick={Message_sender}>send</button>
       </div>
     </main>
     </>
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Server } from 'socket.io'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req, res){
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('input-change', msg => {
+        socket.broadcast.emit('update-input', msg)
+        console.log(msg)
+      })
+    })
+  }
+  res.end()
 }
